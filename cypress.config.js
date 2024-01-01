5dbe5e7bf9f3eb97b8c90d618a18109dff99ee99
nadirbasalamah <nadir008basalamah@gmail.com>
2024-01-01 08:15:57 +0700

    chore(app): remove additional files and configs

--- cypress.config.js
+++ cypress.config.js
@@ -1,29 +1,7 @@
 const { defineConfig } = require("cypress");
-const {
-  addCucumberPreprocessorPlugin,
-} = require("@badeball/cypress-cucumber-preprocessor");
-
-const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
-
-const {
-  createEsbuildPlugin,
-} = require("@badeball/cypress-cucumber-preprocessor/esbuild");
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: "https://www.saucedemo.com",
-    specPattern: "**/*.feature",
-    async setupNodeEvents(on, config) {
-      await addCucumberPreprocessorPlugin(on, config);
-
-      on(
-        "file:preprocessor",
-        createBundler({
-          plugins: [createEsbuildPlugin(config)],
-        })
-      );
-
-      return config;
-    },
   },
 });
--- cypress/e2e/features/logout.feature
+++
@@ -1,9 +0,0 @@
-Feature: Logout
-    As a user
-    I can Logout
-    So that I can exit from the page
-
-    Scenario: Logout successfully
-        Given I am logged in
-        When I click logout button
-        Then I am redirected to the login page
